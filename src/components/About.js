17cb2b03796c3d66f07ff15f48d1081861264d8f
SteveO <stinkypiece@yahoo.com>
2020-12-04 12:24:38 -0800

    public url

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ const About = () => {
     <p style={{ textAlign: 'center'}}>My name is Steve O’Connor and I aim to be a proficient full stack developer.  I’ve always been passionate about problem solving and working in a team environment.  I was fortunate to have a successful career as a classical musician and hope to use some of those skills I honed toward web development.  I really enjoy building things, whether it’s a web application or a piece of furniture, as well as spending time with my wife and son.</p>
 
     <br/>
-    <img src="/images/jack.jpeg" alt="baby" style={{display: 'block', marginLeft: 'auto', marginRight: 'auto' }}/>
+    <img src="%PUBLIC_URL%/images/jack.jpeg" alt="baby" style={{display: 'block', marginLeft: 'auto', marginRight: 'auto' }}/>
     </div>
 
 
